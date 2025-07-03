8d7d70a350cdfdb2b8dc63aaca39c5573dce4510
miintto <miintto.log@gmail.com>
2025-07-03 23:31:44 +0900

    feat: 로깅 추가

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,2 +1,3 @@
 pub mod db;
+pub mod logging;
 pub mod settings;
--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,24 @@ mod entity;
 mod repository;
 mod service;
 
-use axum::{
-    Router,
-    routing::{get, post},
-};
+use axum::{Router, routing::{get, post}};
 use sea_orm::DatabaseConnection;
+use tracing::info;
 
-use config::db::{init_db};
+use config::{
+    db::{init_db},
+    logging::{layer::get_trace_layer, registry::init_logging},
+};
 use service::{auth::{login, register}, user::{get_user, get_user_list}};
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
 
+    init_logging().await;
+
     let conn: DatabaseConnection = init_db().await;
+    info!("Connect Database!");
 
     let user_router = Router::new()
         .route("/", get(get_user_list))
@@ -29,6 +33,7 @@ async fn main() {
         .route("/auth/login", post(login))
         .route("/auth/register", post(register))
         .nest("/users", user_router)
+        .layer(get_trace_layer())
         .with_state(conn);
 
     let listner = tokio::net::TcpListener::bind("127.0.0.1:8000")
